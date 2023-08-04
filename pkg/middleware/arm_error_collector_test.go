2e699d00676eb8c9505a09d240f15a8405c955a1
Peng Gao <gaopeng@microsoft.com>
2023-08-04 03:21:10 +0000

    test: don't overshadow testing.T

--- pkg/middleware/arm_error_collector_test.go
+++ pkg/middleware/arm_error_collector_test.go
@@ -21,39 +21,39 @@ func TestArmRequestMetrics(t *testing.T) {
 	resourceName := "test"
 	testCorrelationId := "testCorrelationId"
 
-	checkRequestInfo := func(iReq *RequestInfo) {
-		assert.NotNil(t, iReq)
-		assert.Equal(t, iReq.ArmResId.SubscriptionID, subID)
-		assert.Equal(t, iReq.ArmResId.ResourceGroupName, rgName)
-		assert.Equal(t, iReq.ArmResId.Name, resourceName)
-		assert.NotNil(t, iReq.Request)
+	checkRequestInfo := func(tt *testing.T, iReq *RequestInfo) {
+		assert.NotNil(tt, iReq)
+		assert.Equal(tt, iReq.ArmResId.SubscriptionID, subID)
+		assert.Equal(tt, iReq.ArmResId.ResourceGroupName, rgName)
+		assert.Equal(tt, iReq.ArmResId.Name, resourceName)
+		assert.NotNil(tt, iReq.Request)
 	}
 
-	checkResponseInfo := func(iResp *ResponseInfo, expectError bool) {
+	checkResponseInfo := func(tt *testing.T, iResp *ResponseInfo, expectError bool) {
 		if expectError {
-			assert.NotNil(t, iResp.Error)
+			assert.NotNil(tt, iResp.Error)
 		} else {
-			assert.Nil(t, iResp.Error)
+			assert.Nil(tt, iResp.Error)
 		}
-		assert.NotNil(t, iResp)
-		assert.NotNil(t, iResp.Response)
-		assert.NotEmpty(t, iResp.RequestId)
-		assert.NotEmpty(t, iResp.CorrelationId)
-		assert.True(t, iResp.Latency > 0)
+		assert.NotNil(tt, iResp)
+		assert.NotNil(tt, iResp.Response)
+		assert.NotEmpty(tt, iResp.RequestId)
+		assert.NotEmpty(tt, iResp.CorrelationId)
+		assert.True(tt, iResp.Latency > 0)
 		connTracking := iResp.ConnTracking
-		assert.NotNil(t, connTracking.ReqConnInfo)
+		assert.NotNil(tt, connTracking.ReqConnInfo)
 		_, err := time.ParseDuration(connTracking.TotalLatency)
-		assert.Nil(t, err)
+		assert.Nil(tt, err)
 		_, err = time.ParseDuration(connTracking.TlsLatency)
-		assert.Nil(t, err)
+		assert.Nil(tt, err)
 		_, err = time.ParseDuration(connTracking.ConnLatency)
-		assert.Nil(t, err)
+		assert.Nil(tt, err)
 		// we don't have a DNS lookup for httptest server so this should be empty
-		assert.Equal(t, connTracking.DnsLatency, "")
+		assert.Equal(tt, connTracking.DnsLatency, "")
 	}
 
-	t.Run("should call RequestStarted and RequestFinished for succeeded requests", func(t *testing.T) {
-		t.Parallel()
+	t.Run("should call RequestStarted and RequestFinished for succeeded requests", func(tt *testing.T) {
+		tt.Parallel()
 		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 		}))
@@ -64,11 +64,11 @@ func TestArmRequestMetrics(t *testing.T) {
 		collector := &testCollector{
 			requestStarted: func(iReq *RequestInfo) {
 				requestedStartedCalled = true
-				checkRequestInfo(iReq)
+				checkRequestInfo(tt, iReq)
 			},
 			requestCompleted: func(iReq *RequestInfo, iResp *ResponseInfo) {
 				requestCompletetedCalled = true
-				checkResponseInfo(iResp, false)
+				checkResponseInfo(tt, iResp, false)
 			},
 		}
 
@@ -77,19 +77,19 @@ func TestArmRequestMetrics(t *testing.T) {
 
 		clientOptions.Transport = newMockServerTransportWithTestServer(ts)
 		client, err := armcontainerservice.NewManagedClustersClient(subID, &mockTokenCredential{}, clientOptions)
-		assert.NoError(t, err)
+		assert.NoError(tt, err)
 		reqHeader := http.Header{}
 		reqHeader.Set("X-Ms-Correlation-Request-Id", testCorrelationId)
 		ctx := runtime.WithHTTPHeader(context.Background(), reqHeader)
 		_, err = client.Get(ctx, rgName, resourceName, nil)
-		assert.NoError(t, err)
+		assert.NoError(tt, err)
 
-		assert.True(t, requestedStartedCalled)
-		assert.True(t, requestCompletetedCalled)
+		assert.True(tt, requestedStartedCalled)
+		assert.True(tt, requestCompletetedCalled)
 	})
 
-	t.Run("should get ArmError for failed requests", func(t *testing.T) {
-		t.Parallel()
+	t.Run("should get ArmError for failed requests", func(tt *testing.T) {
+		tt.Parallel()
 		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(`{"error":{"code":"TestInternalError","message":"The is test internal error."}}`))
@@ -98,14 +98,14 @@ func TestArmRequestMetrics(t *testing.T) {
 
 		collector := &testCollector{
 			requestStarted: func(iReq *RequestInfo) {
-				checkRequestInfo(iReq)
+				checkRequestInfo(tt, iReq)
 			},
 			requestCompleted: func(iReq *RequestInfo, iResp *ResponseInfo) {
-				checkResponseInfo(iResp, true)
+				checkResponseInfo(tt, iResp, true)
 				respErr := iResp.Error
-				assert.NotNil(t, respErr)
-				assert.Equal(t, respErr.Code, "TestInternalError")
-				assert.NotEmpty(t, respErr.Message)
+				assert.NotNil(tt, respErr)
+				assert.Equal(tt, respErr.Code, "TestInternalError")
+				assert.NotEmpty(tt, respErr.Message)
 			},
 		}
 
@@ -117,13 +117,13 @@ func TestArmRequestMetrics(t *testing.T) {
 		reqHeader := http.Header{}
 		reqHeader.Set("X-Ms-Correlation-Request-Id", testCorrelationId)
 		ctx := runtime.WithHTTPHeader(context.Background(), reqHeader)
-		assert.NoError(t, err)
+		assert.NoError(tt, err)
 		_, err = client.Get(ctx, rgName, resourceName, nil)
-		assert.Error(t, err)
+		assert.Error(tt, err)
 	})
 
-	t.Run("should get correct ArmError when context timeout", func(t *testing.T) {
-		t.Parallel()
+	t.Run("should get correct ArmError when context timeout", func(tt *testing.T) {
+		tt.Parallel()
 		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			time.Sleep(1 * time.Second)
 			w.WriteHeader(http.StatusOK)
@@ -132,15 +132,15 @@ func TestArmRequestMetrics(t *testing.T) {
 
 		collector := &testCollector{
 			requestStarted: func(iReq *RequestInfo) {
-				checkRequestInfo(iReq)
+				checkRequestInfo(tt, iReq)
 			},
 			requestCompleted: func(iReq *RequestInfo, iResp *ResponseInfo) {
-				assert.NotNil(t, iResp)
-				assert.Nil(t, iResp.Response)
-				assert.True(t, iResp.Latency > 0)
+				assert.NotNil(tt, iResp)
+				assert.Nil(tt, iResp.Response)
+				assert.True(tt, iResp.Latency > 0)
 				respErr := iResp.Error
-				assert.NotNil(t, respErr)
-				assert.Equal(t, "ContextTimeout", respErr.Code)
+				assert.NotNil(tt, respErr)
+				assert.Equal(tt, "ContextTimeout", respErr.Code)
 			},
 		}
 
@@ -149,16 +149,16 @@ func TestArmRequestMetrics(t *testing.T) {
 		clientOptions.Retry.MaxRetries = -1
 		clientOptions.Transport = newMockServerTransportWithTestServer(ts)
 		client, err := armcontainerservice.NewManagedClustersClient(subID, &mockTokenCredential{}, clientOptions)
-		assert.NoError(t, err)
+		assert.NoError(tt, err)
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
 		defer cancel()
 		_, err = client.Get(ctx, rgName, resourceName, nil)
-		assert.Error(t, err)
-		assert.EqualError(t, err, "context deadline exceeded")
+		assert.Error(tt, err)
+		assert.EqualError(tt, err, "context deadline exceeded")
 	})
 
-	t.Run("should get correct ArmError when context canceled", func(t *testing.T) {
-		t.Parallel()
+	t.Run("should get correct ArmError when context canceled", func(tt *testing.T) {
+		tt.Parallel()
 		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			time.Sleep(1 * time.Second)
 			w.WriteHeader(http.StatusOK)
@@ -167,15 +167,15 @@ func TestArmRequestMetrics(t *testing.T) {
 
 		collector := &testCollector{
 			requestStarted: func(iReq *RequestInfo) {
-				checkRequestInfo(iReq)
+				checkRequestInfo(tt, iReq)
 			},
 			requestCompleted: func(iReq *RequestInfo, iResp *ResponseInfo) {
-				assert.NotNil(t, iResp)
-				assert.Nil(t, iResp.Response)
-				assert.True(t, iResp.Latency > 0)
+				assert.NotNil(tt, iResp)
+				assert.Nil(tt, iResp.Response)
+				assert.True(tt, iResp.Latency > 0)
 				respErr := iResp.Error
-				assert.NotNil(t, respErr)
-				assert.Equal(t, "ContextCanceled", respErr.Code)
+				assert.NotNil(tt, respErr)
+				assert.Equal(tt, "ContextCanceled", respErr.Code)
 			},
 		}
 
@@ -184,15 +184,15 @@ func TestArmRequestMetrics(t *testing.T) {
 		clientOptions.Retry.MaxRetries = -1
 		clientOptions.Transport = newMockServerTransportWithTestServer(ts)
 		client, err := armcontainerservice.NewManagedClustersClient(subID, &mockTokenCredential{}, clientOptions)
-		assert.NoError(t, err)
+		assert.NoError(tt, err)
 		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Millisecond)
 		cancel()
 		_, err = client.Get(ctx, rgName, resourceName, nil)
-		assert.EqualError(t, err, "context canceled")
+		assert.EqualError(tt, err, "context canceled")
 	})
 
-	t.Run("should get correct ArmError for transport error", func(t *testing.T) {
-		t.Parallel()
+	t.Run("should get correct ArmError for transport error", func(tt *testing.T) {
+		tt.Parallel()
 		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 		}))
@@ -201,16 +201,16 @@ func TestArmRequestMetrics(t *testing.T) {
 
 		collector := &testCollector{
 			requestStarted: func(iReq *RequestInfo) {
-				checkRequestInfo(iReq)
+				checkRequestInfo(tt, iReq)
 			},
 			requestCompleted: func(iReq *RequestInfo, iResp *ResponseInfo) {
-				assert.NotNil(t, iResp)
-				assert.Nil(t, iResp.Response)
-				assert.True(t, iResp.Latency > 0)
+				assert.NotNil(tt, iResp)
+				assert.Nil(tt, iResp.Response)
+				assert.True(tt, iResp.Latency > 0)
 				respErr := iResp.Error
-				assert.NotNil(t, respErr)
-				assert.Equal(t, "TransportError", respErr.Code)
-				assert.NotEmpty(t, respErr.Message)
+				assert.NotNil(tt, respErr)
+				assert.Equal(tt, "TransportError", respErr.Code)
+				assert.NotEmpty(tt, respErr.Message)
 			},
 		}
 
@@ -219,13 +219,13 @@ func TestArmRequestMetrics(t *testing.T) {
 		clientOptions.Retry.MaxRetries = -1
 		clientOptions.Transport = newMockServerTransportWithTestServer(ts)
 		client, err := armcontainerservice.NewManagedClustersClient(subID, &mockTokenCredential{}, clientOptions)
-		assert.NoError(t, err)
+		assert.NoError(tt, err)
 		_, err = client.Get(context.Background(), rgName, resourceName, nil)
-		assert.Error(t, err)
+		assert.Error(tt, err)
 	})
 
-	t.Run("should get correct ArmError for server timeout", func(t *testing.T) {
-		t.Parallel()
+	t.Run("should get correct ArmError for server timeout", func(tt *testing.T) {
+		tt.Parallel()
 		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			time.Sleep(1 * time.Second)
 			w.WriteHeader(http.StatusOK)
@@ -234,16 +234,16 @@ func TestArmRequestMetrics(t *testing.T) {
 
 		collector := &testCollector{
 			requestStarted: func(iReq *RequestInfo) {
-				checkRequestInfo(iReq)
+				checkRequestInfo(tt, iReq)
 			},
 			requestCompleted: func(iReq *RequestInfo, iResp *ResponseInfo) {
-				assert.NotNil(t, iResp)
-				assert.Nil(t, iResp.Response)
-				assert.True(t, iResp.Latency > 0)
+				assert.NotNil(tt, iResp)
+				assert.Nil(tt, iResp.Response)
+				assert.True(tt, iResp.Latency > 0)
 				respErr := iResp.Error
-				assert.NotNil(t, respErr)
-				assert.Equal(t, "ContextTimeout", respErr.Code)
-				assert.NotEmpty(t, respErr.Message)
+				assert.NotNil(tt, respErr)
+				assert.Equal(tt, "ContextTimeout", respErr.Code)
+				assert.NotEmpty(tt, respErr.Message)
 			},
 		}
 
@@ -253,19 +253,11 @@ func TestArmRequestMetrics(t *testing.T) {
 		clientOptions.Retry.TryTimeout = 10 * time.Millisecond
 		clientOptions.Transport = newMockServerTransportWithTestServer(ts)
 		client, err := armcontainerservice.NewManagedClustersClient(subID, &mockTokenCredential{}, clientOptions)
-		assert.NoError(t, err)
+		assert.NoError(tt, err)
 		_, err = client.Get(context.Background(), rgName, resourceName, nil)
-		assert.Error(t, err)
+		assert.Error(tt, err)
 	})
 
-	// _, err = client.Get(context.Background(), "test", "test", nil)
-	// here the error is parsed from response body twice
-	// 1. by ArmRequestMetricPolicy, parse and log, and throw away.
-	// 2. by generated client function: runtime.HasStatusCode, and return to here.
-	//assert.Error(t, err)
-
-	//respErr := &azcore.ResponseError{}
-	//assert.True(t, errors.As(err, &respErr))
 }
 
 var _ ArmRequestMetricCollector = &testCollector{}
