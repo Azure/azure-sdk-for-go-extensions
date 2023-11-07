354dc9eb652b37fb3b52d14a9843fe11b6482917
Nick Keller <nikelle@microsoft.com>
2023-11-07 13:20:08 -0500

    Remove references to TLS1.2, to make compatible with TLS1.3 (#35)
    * Remove references to TLS1.2, to make compatible with TLS1.3
    
    * Remove blank tls config struct

--- pkg/middleware/default_http_client.go
+++ pkg/middleware/default_http_client.go
@@ -15,7 +15,6 @@ limitations under the License.
 package middleware
 
 import (
-	"crypto/tls"
 	"net"
 	"net/http"
 	"time"
@@ -54,9 +53,6 @@ func init() {
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
-		TLSClientConfig: &tls.Config{
-			MinVersion: tls.VersionTLS12,
-		},
 	}
 	// We call configureHttp2TransportPing() in the package init to ensure that our defaultTransport is always configured
 	// with the http2 additional settings that work around the issue described here:
@@ -74,7 +70,10 @@ func init() {
 	})
 
 	defaultHTTPClient = &http.Client{
-		Transport: otelhttp.NewTransport(defaultRoundTripper, otelhttp.WithPropagators(propagation.TraceContext{})),
+		Transport: otelhttp.NewTransport(
+			defaultRoundTripper,
+			otelhttp.WithPropagators(propagation.TraceContext{}),
+		),
 	}
 }
 
